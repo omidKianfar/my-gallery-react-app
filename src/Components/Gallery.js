4e670259263b265ee54c4562fc540efa6864add0
omid kianfar <kianfar.omid.990@gmail.com>
2022-06-21 05:03:50 -0700

    show image modal when choose image

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,24 +1,35 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 import Styles from "./Gallery.module.css";
 
 import { Images } from "./ImageSource/Images";
 
 const Gallery = () => {
+  const [model, setModel] = useState(false);
   const [tempImgSrc, setTempImgSrc] = useState("");
 
+  const getImg = (img) => {
+    setTempImgSrc(img);
+    setModel(true);  }
+
   return (
-    <div className={Styles.Gallery}>
+    <Fragment>
+      <div className={model ? `${Styles.modal} ${Styles.modalOpen} `: Styles.modal}>
+        <img src={tempImgSrc}  alt={tempImgSrc}/>
+      </div>
+     <div className={Styles.Gallery}>
       {Images.map((image) => (
         <div className={Styles.Images} key={image.id}>
           <img
             src={image.img}
             alt={image.img}
-            onClick={() => setTempImgSrc(image.img)}
+            onClick={() => getImg(image.img)}
           />
         </div>
       ))}
-    </div>
+    </div> 
+    </Fragment>
+    
   );
 };
 

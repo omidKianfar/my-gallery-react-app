8499d0fcf76e6cf3e0a3b97c604f63d70a54d37c
omid kianfar <kianfar.omid.990@gmail.com>
2022-06-20 23:35:30 -0700

    App.js head and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Styles from './App.module.css';
+
 import Gallery from "./Components/Gallery";
 
 function App() {
   return (
-    <div >
+    <div>
+      <h1 className={Styles.textCenter}>Image Gallery</h1>
+      <h2 className={Styles.textCenter}>Please  click on image to see better that</h2>
       <Gallery />
     </div>
   );
